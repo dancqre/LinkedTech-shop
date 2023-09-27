e7507eb34c0972b1e929c7cce8a57ade11fe34dd
dancqre <94985901+dancqre@users.noreply.github.com>
2023-09-27 04:13:23 +0300

    hover and svg images

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   slidesPerView: 1,
+//   loop: true,
+//   autoplay: {
+//    delay: 1000,
+//   },
+//   effect: 'fade',
+// });
