e453f181116b370935248d4b39fd4a4868830fb6
dancqre <94985901+dancqre@users.noreply.github.com>
2023-09-27 15:19:59 +0300

    item's filter

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   slidesPerView: 1,
-//   loop: true,
-//   autoplay: {
-//    delay: 1000,
-//   },
-//   effect: 'fade',
-// });
+const filterBox = document.querySelectorAll('.buy__item');
+const list  = document.querySelectorAll('.filter-li');
+
+
+list.forEach((li) => {
+  li.addEventListener('click', function () {
+    // Видаляємо клас "activated" у всіх кнопок
+    list.forEach((btn) => {
+      btn.classList.remove('activated');
+    });
+
+    // Додаємо клас "activated" тільки до натиснутої кнопки
+    this.classList.add('activated');
+  });
+});
+ 
+document.querySelector('.filter').addEventListener('click', event => {
+	if (event.target.tagName !== 'LI') return false;
+	let filterClass = event.target.dataset['f'];
+	
+	filterBox.forEach( elem => {
+		elem.classList.remove('hide');
+		if (!elem.classList.contains(filterClass)){
+			elem.classList.add("hide");
+
+		}
+	})
+
+});
