dc7a915bacd7f8edf0298d102570f81fbebcd909
dancqre <94985901+dancqre@users.noreply.github.com>
2023-10-04 02:39:47 +0300

    header adaptive and burger menu

--- js/main.js
+++ js/main.js
@@ -89,3 +89,17 @@ buyItemBtns.forEach(function (buyItemBtn) {
   });
 });
 	
+//Burger menu
+
+const burger = document.querySelector('.menu-icon');
+const menu = document.querySelector('.nav__menu');
+const body = document.body;
+
+if (burger && menu) {
+	burger.addEventListener('click', () => {
+		burger.classList.toggle('_active');
+		menu.classList.toggle('_active');
+		body.classList.toggle('_lock');
+	})
+}
+
